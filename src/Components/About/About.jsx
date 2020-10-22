89fc4e0f500d9981612056d8909f11ece6aa4ce9
ladeirakelwin <ladeirakelwin@gmail.com>
2020-10-22 15:08:09 -0300

    Added About

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { about, title, main, image, text } from "./About.module.css";
+import img from '../../assets/img/about-pic.jpg'
+
+const About = () => {
+  return (
+    <section className={about}>
+      <div class={title}>
+        <h1>Sobre Nós</h1>
+      </div>
+      <section className={main}>
+        <div className={image}>
+          <img src={img} alt="logo" />
+        </div>
+        <div className={text}>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.</p>
+        </div>
+      </section>
+    </section>
+  )
+}
+
+export default About
+
